beca47bc57c0605197bd03d5fb44482fad7fbc6d
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-14 13:14:48 +0300

    declaring a winner

--- 01. React documentation/01. Getting Started/quick-start/src/components/Tic-Tac-Toe/TicTacToe.js
+++ 01. React documentation/01. Getting Started/quick-start/src/components/Tic-Tac-Toe/TicTacToe.js
@@ -1,35 +1,82 @@
+import { useState } from "react";
 import styles from "./TicTacToe.module.css";
 
-function Square({ value }) {
-  function handleClick() {
-    console.log("first");
-  }
-
+function Square({ value, onSquareClick }) {
   return (
-    <button onClick={handleClick} className={styles.square}>
+    <button className={styles.square} onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(index) {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status = "";
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <>
+      <div className={styles.status}>{status}</div>
       <div className={styles["board-row"]}>
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className={styles["board-row"]}>
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className={styles["board-row"]}>
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+// In React, it’s conventional to use onSomething names for props which represent
+// events and handleSomething for the function definitions which handle those events.
