f06561a01b53e11c65397fea29d38e389a750f67
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-23 14:45:09 +0300

    Passing event handlers as props

---
+++ 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/RespondingToEvents/PassingEventHandlersAsProps/PassingEventHandlersAsProps.js
@@ -0,0 +1,33 @@
+export default function PassingEventHandlersAsProps() {
+  return (
+    <>
+      <h2>Passing event handlers as props</h2>
+      <Toolbar />
+    </>
+  );
+}
+
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+function PlayButton({ movieName }) {
+  function handlePlayClick() {
+    alert(`Playing ${movieName}!`);
+  }
+
+  return <Button onClick={handlePlayClick}>Play "{movieName}"</Button>;
+}
+
+function UploadButton() {
+  return <Button onClick={() => alert("Uploading!")}>Upload Image</Button>;
+}
+
+function Toolbar() {
+  return (
+    <div>
+      <PlayButton movieName="Kiki's Delivery Service" />
+      <UploadButton />
+    </div>
+  );
+}
--- 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/RespondingToEvents/RespondingToEvents.js
+++ 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/RespondingToEvents/RespondingToEvents.js
@@ -1,11 +1,13 @@
-import ReadingProps from "./ReadingProps/ReadingProps";
+import PassingEventHandlersAsProps from "./PassingEventHandlersAsProps/PassingEventHandlersAsProps";
+// import ReadingProps from "./ReadingProps/ReadingProps";
 // import AddingEventHandlers from "./AddingEventHandlers/AddingEventHandlers";
 
 export default function RespondingToEvents() {
   return (
     <>
       <h1>Responding to events</h1>
-      <ReadingProps />
+      <PassingEventHandlersAsProps />
+      {/* <ReadingProps /> */}
       {/* <AddingEventHandlers /> */}
     </>
   );
