7e42b949a87240736b05e177daf7c78c910a8795
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-24 15:11:31 +0300

    Giving a component multiple state variables

--- 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/State/VariableIsNotEnough/VariableIsNotEnough.js
+++ 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/State/VariableIsNotEnough/VariableIsNotEnough.js
@@ -12,9 +12,14 @@ export default function VariableIsNotEnough() {
 
 function Gallery() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   let sculpture = sculptureList[index];
@@ -29,8 +34,11 @@ function Gallery() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
     </>
   );
 }
