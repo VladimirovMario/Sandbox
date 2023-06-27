ad630e010fe897e7dd84ef6e93524fd6c8c37e6d
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-27 15:49:50 +0300

    Copying objects with the spread syntax

--- 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/UpdatingObjectsInState/UpdatingObjectsInState.js
+++ 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/UpdatingObjectsInState/UpdatingObjectsInState.js
@@ -1,10 +1,12 @@
-import TreatStateAsReadOnly from './TreatStateAsReadOnly/TreatStateAsReadOnly';
+import CopyingObjectsSpreadSyntax from './CopyingObjectsSpreadSyntax/CopyingObjectsSpreadSyntax';
+// import TreatStateAsReadOnly from './TreatStateAsReadOnly/TreatStateAsReadOnly';
 
 export default function UpdatingObjectsInState() {
   return (
     <>
       <h1>Updating Objects in State</h1>
-      <TreatStateAsReadOnly />
+      <CopyingObjectsSpreadSyntax />
+      {/* <TreatStateAsReadOnly /> */}
     </>
   );
 }
