0b7f010e4a8bbc1dd666d41e3bc643cf12f7fb70
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-27 16:12:13 +0300

    Updating a nested object

--- 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/UpdatingObjectsInState/UpdatingObjectsInState.js
+++ 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/UpdatingObjectsInState/UpdatingObjectsInState.js
@@ -1,11 +1,13 @@
-import CopyingObjectsSpreadSyntax from './CopyingObjectsSpreadSyntax/CopyingObjectsSpreadSyntax';
+import UpdatingANestedObject from './UpdatingANestedObject/UpdatingANestedObject';
+// import CopyingObjectsSpreadSyntax from './CopyingObjectsSpreadSyntax/CopyingObjectsSpreadSyntax';
 // import TreatStateAsReadOnly from './TreatStateAsReadOnly/TreatStateAsReadOnly';
 
 export default function UpdatingObjectsInState() {
   return (
     <>
       <h1>Updating Objects in State</h1>
-      <CopyingObjectsSpreadSyntax />
+      <UpdatingANestedObject />
+      {/* <CopyingObjectsSpreadSyntax /> */}
       {/* <TreatStateAsReadOnly /> */}
     </>
   );
