5c479d79a59e607e602c3ad6398daa60dca26dae
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-28 13:12:20 +0300

    Transforming an array

--- 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/UpdatingArraysInState/UpdatingArraysInState.js
+++ 01. React documentation/02. Learn React/02. Adding Interactivity/src/components/UpdatingArraysInState/UpdatingArraysInState.js
@@ -1,11 +1,13 @@
-import RemovingFromAnArray from './RemovingFromAnArray/RemovingFromAnArray';
+import TransformingAnArray from './TransformingAnArray/TransformingAnArray';
+// import RemovingFromAnArray from './RemovingFromAnArray/RemovingFromAnArray';
 // import AddingToAnArray from './AddingToAnArray/AddingToAnArray';
 
 export default function UpdatingArraysInState() {
   return (
     <>
       <h1>Updating Arrays in State</h1>
-      <RemovingFromAnArray />
+      <TransformingAnArray />
+      {/* <RemovingFromAnArray /> */}
       {/* <AddingToAnArray /> */}
     </>
   );
