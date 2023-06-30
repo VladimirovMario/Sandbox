3549f734a2d4776f9ef15514a4e07fd7299fa1e3
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-30 16:45:21 +0300

    Recap

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ReactingToInputWithState/ReactingToInputWithState.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ReactingToInputWithState/ReactingToInputWithState.js
@@ -8,3 +8,16 @@ export default function ReactingToInputWithState() {
     </>
   );
 }
+
+/*
+Recap
+
+    Declarative programming means describing the UI for each visual state rather than micromanaging the UI (imperative).
+    When developing a component:
+        Identify all its visual states.
+        Determine the human and computer triggers for state changes.
+        Model the state with useState.
+        Remove non-essential state to avoid bugs and paradoxes.
+        Connect the event handlers to set state.
+
+*/
