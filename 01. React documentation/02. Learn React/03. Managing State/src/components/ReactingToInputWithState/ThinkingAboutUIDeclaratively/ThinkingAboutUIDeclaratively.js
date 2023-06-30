df02e05a4f33671a69a5ebe3fb5662063b45c24d
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-30 16:28:05 +0300

    Created event handlers that update the state

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ReactingToInputWithState/ThinkingAboutUIDeclaratively/ThinkingAboutUIDeclaratively.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ReactingToInputWithState/ThinkingAboutUIDeclaratively/ThinkingAboutUIDeclaratively.js
@@ -1,30 +1,38 @@
+import { useState } from 'react';
+
 export default function ThinkingAboutUIDeclaratively() {
   return (
     <>
       <h2>Thinking about UI declaratively</h2>
-      <App />
+      <Form />
     </>
   );
 }
 
-let statuses = ['empty', 'typing', 'submitting', 'success', 'error'];
+function Form() {
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('empty');
 
-function App() {
-  return (
-    <>
-      {statuses.map((status) => (
-        <section key={status}>
-          <h4>Form ({status}):</h4>
-          <Form status={status} />
-        </section>
-      ))}
-    </>
-  );
-}
-
-function Form({ status = 'empty' }) {
   if (status === 'success') {
     return <h1>That's right!</h1>;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setStatus('submitting');
+    try {
+      await submitForm(answer.trim());
+      setStatus('success');
+    } catch (error) {
+      setStatus('typing');
+      setError(error);
+    }
+  }
+
+  function handleTextareaChange(e) {
+    setAnswer(e.target.value);
+    setStatus('typing');
   }
 
   return (
@@ -33,18 +41,49 @@ function Form({ status = 'empty' }) {
       <p>
         In which city is there a billboard that turns air into drinkable water?
       </p>
-      <form>
-        <textarea disabled={status === 'submitting'}></textarea>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handleTextareaChange}
+          disabled={status === 'submitting'}
+        ></textarea>
         <br />
-        <button disabled={status === 'empty' || status === 'submitting'}>
+
+        <button disabled={answer.length === 0 || status === 'submitting'}>
           Submit
         </button>
-        {status === 'error' && (
-          <p style={{ color: 'red' }}>
-            Good guess but a wrong answer. Try again!
-          </p>
-        )}
+
+        {error !== null && <p style={{ color: 'red' }}>{error.message}</p>}
       </form>
     </>
   );
 }
+
+async function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let shouldError = answer.toLowerCase() !== 'lima';
+      if (shouldError) {
+        reject(new Error('Good guess but a wrong answer. Try again!'));
+      } else {
+        resolve();
+      }
+    }, 1500);
+  });
+}
+
+// let statuses = ['empty', 'typing', 'submitting', 'success', 'error'];
+
+// function App() {
+//   return (
+//     <>
+//       {statuses.map((status) => (
+//         <section key={status}>
+//           <h4>Form ({status}):</h4>
+//           <Form status={status} />
+//         </section>
+//       ))}
+//     </>
+//   );
+// }
