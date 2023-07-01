bbda99bd40f9e17858b35852ccf5c0243c1f8241
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-01 17:32:01 +0300

    Avoid duplication in state

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ChoosingTheStateStructure/ChoosingTheStateStructure.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ChoosingTheStateStructure/ChoosingTheStateStructure.js
@@ -1,11 +1,13 @@
-import AvoidRedundantState from './AvoidRedundantState/AvoidRedundantState';
+import AvoidDuplicationInState from './AvoidDuplicationInState/AvoidDuplicationInState';
+// import AvoidRedundantState from './AvoidRedundantState/AvoidRedundantState';
 // import AvoidContradictionsInState from './AvoidContradictionsInState/AvoidContradictionsInState';
 
 export default function ChoosingTheStateStructure() {
   return (
     <>
       <h1>Choosing the State Structure</h1>
-      <AvoidRedundantState />
+      <AvoidDuplicationInState />
+      {/* <AvoidRedundantState /> */}
       {/* <AvoidContradictionsInState /> */}
     </>
   );
