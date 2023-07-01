ef34c0a1e583d59ced4d8eb8239bc9703ae8cd8e
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-01 17:05:22 +0300

    Avoid contradictions in state

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ChoosingTheStateStructure/AvoidContradictionsInState/AvoidContradictionsInState.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ChoosingTheStateStructure/AvoidContradictionsInState/AvoidContradictionsInState.js
@@ -11,16 +11,17 @@ export default function AvoidContradictionsInState() {
 
 function FeedbackForm() {
   const [text, setText] = useState('');
-  const [isSending, setIsSending] = useState(false);
-  const [isSent, setIsSent] = useState(false);
+  const [status, setStatus] = useState('typing');
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setIsSending(true);
+    setStatus('sending');
     await sendMessage(text);
-    setIsSending(false);
-    setIsSent(true);
+    setStatus('sent');
   }
+
+  const isSending = status === 'sending';
+  const isSent = status === 'sent';
 
   if (isSent) {
     return <h1>Thanks for feedback!</h1>;
