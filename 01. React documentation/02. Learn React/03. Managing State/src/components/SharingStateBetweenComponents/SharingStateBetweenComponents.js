0004454a00f3dc4995c4299a8f5f9e19692c968c
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-03 14:23:17 +0300

    Recap

--- 01. React documentation/02. Learn React/03. Managing State/src/components/SharingStateBetweenComponents/SharingStateBetweenComponents.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/SharingStateBetweenComponents/SharingStateBetweenComponents.js
@@ -8,3 +8,13 @@ export default function SharingStateBetweenComponents() {
     </>
   );
 }
+
+/*
+Recap
+
+    When you want to coordinate two components, move their state to their common parent.
+    Then pass the information down through props from their common parent.
+    Finally, pass the event handlers down so that the children can change the parent’s state.
+    It’s useful to consider components as “controlled” (driven by props) or “uncontrolled” (driven by state).
+
+*/
