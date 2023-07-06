2c68d73a47ef0fd94c2f679a871d03f33a1cc884
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-06 11:25:39 +0300

    Replaced useState with useReducer

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ExtractingStateLogicIntoAReducer/ConsolidateStateLogicWithAReducer/ConsolidateStateLogicWithAReducer.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ExtractingStateLogicIntoAReducer/ConsolidateStateLogicWithAReducer/ConsolidateStateLogicWithAReducer.js
@@ -1,7 +1,70 @@
+import { useReducer } from 'react';
+import { tasksData } from './data';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
+
 export default function ConsolidateStateLogicWithAReducer() {
-    return(
-        <>
-        <h2>Consolidate state logic with a reducer</h2>
-        </>
-    );
+  return (
+    <>
+      <h2>Consolidate state logic with a reducer</h2>
+      <TaskApp />
+    </>
+  );
+}
+
+function TaskApp() {
+  const [tasks, dispatch] = useReducer(tasksReducer, tasksData.initialTasks);
+
+  function handleAddTask(text) {
+    dispatch({
+      // "action" object:
+      type: 'added',
+      id: tasksData.nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task) {
+    dispatch({
+      // "action" object:
+      type: 'changed',
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      // "action" object:
+      type: 'delete',
+      id: taskId,
+    });
+  }
+
+  return (
+    <>
+      <h2>Prague itinerary</h2>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
+  );
+}
+
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [...tasks, { id: action.id, text: action.text, done: false }];
+    }
+    case 'changed': {
+      return tasks.map((t) => (t.id === action.task.id ? action.task : t));
+    }
+    case 'delete': {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default:
+      throw Error(`Unknown action: ${action.type}`);
+  }
 }
