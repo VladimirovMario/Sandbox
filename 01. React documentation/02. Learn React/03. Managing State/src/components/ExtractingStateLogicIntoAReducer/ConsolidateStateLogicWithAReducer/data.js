27611b590a435abc2453f0bad2938b98266a84aa
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-06 11:14:37 +0300

    Extracted data.js

---
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ExtractingStateLogicIntoAReducer/ConsolidateStateLogicWithAReducer/data.js
@@ -0,0 +1,8 @@
+export const tasksData = {
+  nextId: 3,
+  initialTasks: [
+    { id: 0, text: 'Visit Kafka Museum', done: true },
+    { id: 1, text: 'Watch a puppet show', done: false },
+    { id: 2, text: 'Lennon Wall pic', done: false },
+  ],
+};
