ff81f43ccd399d34507e4edb947e00dfa7bf631f
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-10 18:46:49 +0300

    Step 3: Use context anywhere in the tree

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ScalingUpWithReducerAndContext/CombiningAReducerWithContext/CombiningAReducerWithContext.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ScalingUpWithReducerAndContext/CombiningAReducerWithContext/CombiningAReducerWithContext.js
@@ -15,38 +15,12 @@ export default function CombiningAReducerWithContext() {
 function TaskApp() {
   const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
 
-  function handleAddTask(text) {
-    dispatch({
-      type: 'added',
-      id: nextId++,
-      text: text,
-    });
-  }
-
-  function handleChangeTask(task) {
-    dispatch({
-      type: 'changed',
-      task: task,
-    });
-  }
-
-  function handleDeleteTask(taskId) {
-    dispatch({
-      type: 'deleted',
-      id: taskId,
-    });
-  }
-
   return (
     <TasksContext.Provider value={tasks}>
       <TasksDispatchContext.Provider value={dispatch}>
         <h3>Day off in Kyoto</h3>
-        <AddTask onAddTask={handleAddTask} />
-        <TaskList
-          tasks={tasks}
-          onChangeTask={handleChangeTask}
-          onDeleteTask={handleDeleteTask}
-        />
+        <AddTask />
+        <TaskList />
       </TasksDispatchContext.Provider>
     </TasksContext.Provider>
   );
@@ -75,7 +49,6 @@ function tasksReducer(tasks, action) {
   }
 }
 
-let nextId = 3;
 const initialTasks = [
   { id: 0, text: 'Philosopher’s Path', done: true },
   { id: 1, text: 'Visit the temple', done: false },
