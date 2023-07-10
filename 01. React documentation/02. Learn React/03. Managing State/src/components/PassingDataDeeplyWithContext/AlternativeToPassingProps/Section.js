ce548f66673b8a1ad0f1e3655a3ed7743b34a97a
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-10 14:17:43 +0300

    Provide the context

--- 01. React documentation/02. Learn React/03. Managing State/src/components/PassingDataDeeplyWithContext/AlternativeToPassingProps/Section.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/PassingDataDeeplyWithContext/AlternativeToPassingProps/Section.js
@@ -1,3 +1,9 @@
-export default function Section({ children }) {
-  return <section className="section">{children}</section>;
+import { LevelContext } from './LevelContext';
+
+export default function Section({ level, children }) {
+  return (
+    <LevelContext.Provider value={level}>
+      <section className="section">{children}</section>
+    </LevelContext.Provider>
+  );
 }
