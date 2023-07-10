233b27e665b1eb347e0d4f1afaa7ea5c82d7bffe
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-10 19:10:34 +0300

    Recap

--- 01. React documentation/02. Learn React/03. Managing State/src/components/ScalingUpWithReducerAndContext/ScalingUpWithReducerAndContext.js
+++ 01. React documentation/02. Learn React/03. Managing State/src/components/ScalingUpWithReducerAndContext/ScalingUpWithReducerAndContext.js
@@ -8,3 +8,17 @@ export default function ScalingUpWithReducerAndContext() {
     </>
   );
 }
+
+/*
+Recap
+
+    You can combine reducer with context to let any component read and update state above it.
+    To provide state and the dispatch function to components below:
+        Create two contexts (for state and for dispatch functions).
+        Provide both contexts from the component that uses the reducer.
+        Use either context from components that need to read them.
+    You can further declutter the components by moving all wiring into one file.
+        You can export a component like TasksProvider that provides context.
+        You can also export custom Hooks like useTasks and useTasksDispatch to read it.
+    You can have many context-reducer pairs like this in your app.
+*/
