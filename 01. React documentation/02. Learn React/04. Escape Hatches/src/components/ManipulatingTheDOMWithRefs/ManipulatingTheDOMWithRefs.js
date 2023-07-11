9e58852e959895a0a975f29ca834f6b611edf447
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-11 16:25:59 +0300

    Recap

--- 01. React documentation/02. Learn React/04. Escape Hatches/src/components/ManipulatingTheDOMWithRefs/ManipulatingTheDOMWithRefs.js
+++ 01. React documentation/02. Learn React/04. Escape Hatches/src/components/ManipulatingTheDOMWithRefs/ManipulatingTheDOMWithRefs.js
@@ -18,3 +18,14 @@ export default function ManipulatingTheDOMWithRefs() {
     </>
   );
 }
+
+/*
+Recap
+
+    Refs are a generic concept, but most often you’ll use them to hold DOM elements.
+    You instruct React to put a DOM node into myRef.current by passing <div ref={myRef}>.
+    Usually, you will use refs for non-destructive actions like focusing, scrolling, or measuring DOM elements.
+    A component doesn’t expose its DOM nodes by default. You can opt into exposing a DOM node by using forwardRef and passing the second ref argument down to a specific node.
+    Avoid changing DOM nodes managed by React.
+    If you do modify DOM nodes managed by React, modify parts that React has no reason to update.
+*/
