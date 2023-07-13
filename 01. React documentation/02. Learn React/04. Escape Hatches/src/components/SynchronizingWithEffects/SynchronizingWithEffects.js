ace4502d955ed14cdcde6f1751142effad46fffb
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-13 16:11:40 +0300

    Specify the Effect dependencies

--- 01. React documentation/02. Learn React/04. Escape Hatches/src/components/SynchronizingWithEffects/SynchronizingWithEffects.js
+++ 01. React documentation/02. Learn React/04. Escape Hatches/src/components/SynchronizingWithEffects/SynchronizingWithEffects.js
@@ -1,10 +1,12 @@
-import DeclareAnEffect from './DeclareAnEffect/DeclareAnEffect';
+import SpecifyTheEffectDependencies from './SpecifyTheEffectDependencies/SpecifyTheEffectDependencies';
+// import DeclareAnEffect from './DeclareAnEffect/DeclareAnEffect';
 
 export default function SynchronizingWithEffects() {
   return (
     <>
       <h1>Synchronizing with Effects</h1>
-      <DeclareAnEffect />
+      <SpecifyTheEffectDependencies />
+      {/* <DeclareAnEffect /> */}
     </>
   );
 }
