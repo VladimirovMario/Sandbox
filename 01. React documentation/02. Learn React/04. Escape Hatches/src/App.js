65811136c3d00094b1e591454229cf51f22b830a
VladimirovMario <mariovladimirovfs@gmail.com>
2023-07-17 12:42:05 +0300

    Recap

--- 01. React documentation/02. Learn React/04. Escape Hatches/src/App.js
+++ 01. React documentation/02. Learn React/04. Escape Hatches/src/App.js
@@ -1,11 +1,13 @@
-import SynchronizingWithEffects from './components/SynchronizingWithEffects/SynchronizingWithEffects';
+import YouMightNotNeedAnEffect from './components/YouMightNotNeedAnEffect/YouMightNotNeedAnEffect';
+// import SynchronizingWithEffects from './components/SynchronizingWithEffects/SynchronizingWithEffects';
 // import ManipulatingTheDOMWithRefs from './components/ManipulatingTheDOMWithRefs/ManipulatingTheDOMWithRefs';
 // import ReferencingValuesWithRefs from './components/ReferencingValuesWithRefs/ReferencingValuesWithRefs';
 
 function App() {
   return (
     <div className="App">
-      <SynchronizingWithEffects />
+      <YouMightNotNeedAnEffect />
+      {/* <SynchronizingWithEffects /> */}
       {/* <ManipulatingTheDOMWithRefs /> */}
       {/* <ReferencingValuesWithRefs /> */}
     </div>
