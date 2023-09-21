ac9a322fac9754d0dd1704148383b23a9adf4668
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-21 15:42:55 +0300

    Added PostList component with useSelector hook

---
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/PostsList.js
@@ -0,0 +1,16 @@
+// https://redux.js.org/tutorials/fundamentals/part-5-ui-react#reading-state-from-the-store-with-useselector
+import { useSelector } from 'react-redux';
+import { selectAllPosts } from './postsSlice';
+
+export default function PostsList() {
+  const posts = useSelector(selectAllPosts);
+
+  const renderedPosts = posts.map((post) => (
+    <article key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.content.substring(0, 100)}</p>
+    </article>
+  ));
+
+  return <section>{renderedPosts}</section>;
+}
