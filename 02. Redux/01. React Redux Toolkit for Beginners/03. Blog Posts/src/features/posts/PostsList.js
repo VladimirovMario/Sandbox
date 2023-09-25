1ed0c06ad0e57896ed975b6deac3dbb2a910b3cf
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-25 10:41:40 +0300

    Displaying posts by date added order

--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/PostsList.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/PostsList.js
@@ -7,7 +7,11 @@ import TimeAgo from './TimeAgo';
 export default function PostsList() {
   const posts = useSelector(selectAllPosts);
 
-  const renderedPosts = posts.map((post) => (
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPosts = orderedPosts.map((post) => (
     <article key={post.id}>
       <h2>{post.title}</h2>
       <p>{post.content.substring(0, 100)}</p>
