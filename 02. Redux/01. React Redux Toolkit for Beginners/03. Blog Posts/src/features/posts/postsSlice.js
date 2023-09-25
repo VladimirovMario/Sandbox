ae7ae4308fab6836f19010f1573fe602fa1adc40
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-25 18:55:16 +0300

    feat: Implement Async Actions for Posts
    - Added asynchronous thunk actions for fetching and adding posts.
    - Handled API requests using Axios to retrieve and create posts.
    - Enhanced the 'fetchPosts' action to add dates and reactions to loaded posts.
    - Added error handling for API requests and updated state accordingly.
    - Improved data structure for new posts and added them to the state.

--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/postsSlice.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/postsSlice.js
@@ -1,36 +1,42 @@
 // https://redux-toolkit.js.org/api/createSlice
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+// https://redux.js.org/usage/writing-logic-thunks#thunk-overview
+// https://axios-http.com/
 // https://date-fns.org/
-import { sub } from 'date-fns';
+// https://stackoverflow.com/questions/73444733/redux-toolkit-state-showing-as-proxy-handler-null-target-null-i
+import {
+  createSlice,
+  nanoid,
+  createAsyncThunk,
+  // current,
+} from '@reduxjs/toolkit';
+import axios from 'axios';
+import { sub } from 'date-fns/esm';
 
-const initialState = [
-  {
-    id: 1,
-    title: 'Learning Redux Toolkit',
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: 2,
-    title: 'Slices...',
-    content: 'The more I say slice the more I want pizza.',
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const initialState = {
+  posts: [],
+  status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  try {
+    const response = await axios.get(POSTS_URL);
+    return [...response.data];
+  } catch (error) {
+    return error.message;
+  }
+});
+
+const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPost) => {
+  try {
+    const response = await axios.post(POSTS_URL, initialPost);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+});
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -40,7 +46,7 @@ const postsSlice = createSlice({
     // https://redux-toolkit.js.org/api/createSlice#customizing-generated-action-creators
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -63,15 +69,62 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find((post) => post.id === postId);
+      const existingPost = state.posts.find((post) => post.id === postId);
       if (existingPost) {
         existingPost.reactions[reaction]++;
       }
     },
   },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        // Adding date and reactions
+        let min = 1;
+        const loadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+          return post;
+        });
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts);
+
+        // console.log('before', current(state.posts));
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          wow: 0,
+          heart: 0,
+          rocket: 0,
+          coffee: 0,
+        };
+        console.log(action.payload);
+        state.posts.push(action.payload);
+      });
+  },
 });
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
+export const getPostsStatus = (state) => state.posts.status;
+export const getPostsError = (state) => state.posts.error;
+
+export { fetchPosts, addNewPost };
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
