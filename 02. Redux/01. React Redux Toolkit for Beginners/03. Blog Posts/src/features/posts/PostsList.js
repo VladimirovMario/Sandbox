4a5041d626cba2f19c1418386c722811e15a4495
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-25 09:58:31 +0300

    Added PostAuthor component

--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/PostsList.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/PostsList.js
@@ -1,6 +1,7 @@
 // https://redux.js.org/tutorials/fundamentals/part-5-ui-react#reading-state-from-the-store-with-useselector
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postsSlice';
+import PostAuthor from './PostAuthor';
 
 export default function PostsList() {
   const posts = useSelector(selectAllPosts);
@@ -9,6 +10,9 @@ export default function PostsList() {
     <article key={post.id}>
       <h2>{post.title}</h2>
       <p>{post.content.substring(0, 100)}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+      </p>
     </article>
   ));
 
