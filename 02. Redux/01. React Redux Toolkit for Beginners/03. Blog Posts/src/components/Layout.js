3a7c8449bd2b818ca116f17582d3fbbe57e5ef94
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-26 16:35:28 +0300

    Added Header component

--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/components/Layout.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/components/Layout.js
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 
 export default function Layout() {
   return (
-    <main className="APP">
-      <Outlet />
-    </main>
+    <>
+      <Header />
+      <main className="APP">
+        <Outlet />
+      </main>
+    </>
   );
 }
