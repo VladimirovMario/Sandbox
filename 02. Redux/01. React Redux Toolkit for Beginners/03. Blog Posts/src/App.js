65e679a15fd568b9915965dcf20cf590105e46d8
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-26 17:29:14 +0300

    Added route for EditPost.js

--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/App.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/App.js
@@ -4,6 +4,7 @@ import PostsList from './features/posts/PostsList';
 import SinglePost from './features/posts/SinglePost';
 import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
+import EditPost from './features/posts/EditPost';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="post">
           <Route index element={<AddPostForm />} />
           <Route path=":postId" element={<SinglePost />} />
+          <Route path="edit/:postId" element={<EditPost />} />
         </Route>
       </Route>
     </Routes>
--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/SinglePost.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/features/posts/SinglePost.js
@@ -4,7 +4,7 @@ import { getPostById } from './postsSlice';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
 import ReactionButtons from './ReactionButtons';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function SinglePost() {
   const { postId } = useParams();
@@ -24,6 +24,7 @@ export default function SinglePost() {
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <p className="postCredit">
+        <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
