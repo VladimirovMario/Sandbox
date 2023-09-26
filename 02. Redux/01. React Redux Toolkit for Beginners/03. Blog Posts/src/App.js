b67467f72b1ac117789e0bb2f3f9659f36c3ceb8
VladimirovMario <mariovladimirovfs@gmail.com>
2023-09-26 16:12:41 +0300

    Added react-router-dom to project

--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/App.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/App.js
@@ -1,12 +1,22 @@
 import AddPostForm from './features/posts/AddPostForm';
 import PostsList from './features/posts/PostsList';
 
+import SinglePost from './features/posts/SinglePost';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <AddPostForm />
-      <PostsList />
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePost />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/index.js
+++ 02. Redux/01. React Redux Toolkit for Beginners/03. Blog Posts/src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { fetchUsers } from './features/users/usersSlice';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 store.dispatch(fetchUsers());
 
@@ -13,7 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
