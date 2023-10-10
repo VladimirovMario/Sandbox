25eca7d8bd20b84f269f6d9f2c89e8ea3cd87a6b
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-10 18:08:15 +0300

    Automatic Refreshing with Cache Invalidation

--- 02. Redux/02. Redux Documentation/02. Redux Essentials/redux-essentials-example-app-master/src/features/api/apiSlice.js
+++ 02. Redux/02. Redux Documentation/02. Redux Essentials/redux-essentials-example-app-master/src/features/api/apiSlice.js
@@ -5,16 +5,13 @@ const baseUrl = '/fakeApi';
 
 // Define our single API slice object
 const apiSlice = createApi({
-  // The cache reducer expects to be added at `state.api` (already default - this is optional)
   reducerPath: 'api',
-  // All of our requests will have URLs starting with '/fakeApi'
   baseQuery: fetchBaseQuery({ baseUrl }),
-  // The "endpoints" represent operations and requests for this server
+  tagTypes: ['Post'],
   endpoints: (builder) => ({
-    // The `getPosts` endpoint is a "query" operation that returns data
     getPosts: builder.query({
-      // The URL for the request is '/fakeApi/posts'
       query: () => '/posts',
+      providesTags: ['Post'],
     }),
     getPost: builder.query({
       query: (postId) => `/posts/${postId}`,
@@ -25,6 +22,7 @@ const apiSlice = createApi({
         method: 'POST',
         body: initialPost,
       }),
+      invalidatesTags: ['Post'],
     }),
   }),
 });
