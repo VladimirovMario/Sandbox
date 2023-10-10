8ab62732e191adf453952f8959659469b0f08819
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-10 15:48:48 +0300

    Displaying Posts with Queries - Using Query Hooks in Components

--- 02. Redux/02. Redux Documentation/02. Redux Essentials/redux-essentials-example-app-master/src/features/posts/PostsList.js
+++ 02. Redux/02. Redux Documentation/02. Redux Essentials/redux-essentials-example-app-master/src/features/posts/PostsList.js
@@ -1,36 +1,27 @@
-import { useEffect } from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchPosts, selectPostIds } from './postsSlice';
+import { useGetPostsQuery } from '../api/apiSlice';
 
 import { Spinner } from '../../components/Spinner';
 import { PostExcerpt } from './PostExcerpt';
 
 export const PostsList = () => {
-  const dispatch = useDispatch();
-  const orderedPostIds = useSelector(selectPostIds);
-
-  const postStatus = useSelector((state) => state.posts.status);
-  const error = useSelector((state) => state.posts.error);
-
-  useEffect(() => {
-    if (postStatus === 'idle') {
-      dispatch(fetchPosts());
-    }
-  }, [postStatus, dispatch]);
+  const {
+    data: posts,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetPostsQuery();
 
   let content = <></>;
 
-  if (postStatus === 'loading') {
+  if (isLoading) {
     content = <Spinner text="Loading..." />;
   }
-  if (postStatus === 'succeeded') {
-    content = orderedPostIds.map((postId) => (
-      <PostExcerpt key={postId} postId={postId} />
-    ));
+  if (isSuccess) {
+    content = posts.map((post) => <PostExcerpt key={post.id} post={post} />);
   }
-  if (postStatus === 'error') {
-    content = <div>{error}</div>;
+  if (isError) {
+    content = <div>{error.toString()}</div>;
   }
 
   return (
@@ -40,3 +31,67 @@ export const PostsList = () => {
     </section>
   );
 };
+
+/*
+console.log(useGetPostsQuery());
+
+Object { status: "pending", isUninitialized: false, isLoading: true, isSuccess: false, isError: false, data: undefined, currentData: undefined, isFetching: true, refetch: refetch()...}​
+currentData: undefined​
+data: undefined​
+isError: false​
+isFetching: true​
+isLoading: true​
+isSuccess: false​
+isUninitialized: false​
+refetch: function refetch()​
+status: "pending"​
+<prototype>: Object { … }
+PostsList.js:13
+
+Object { status: "pending", endpointName: "getPosts", requestId: "q62Jtu77cpI_T612SLuhD", startedTimeStamp: 1696941177210, isUninitialized: false, isLoading: true, isSuccess: false, isError: false, data: undefined, currentData: undefined, … }​
+currentData: undefined​
+data: undefined​
+endpointName: "getPosts"​
+isError: false​
+isFetching: true​
+isLoading: true​
+isSuccess: false​
+isUninitialized: false​
+refetch: function refetch()​
+requestId: "q62Jtu77cpI_T612SLuhD"​
+startedTimeStamp: 1696941177210​
+status: "pending"​
+<prototype>: Object { … }
+PostsList.js:13
+
+Object { status: "pending", endpointName: "getPosts", requestId: "q62Jtu77cpI_T612SLuhD", startedTimeStamp: 1696941177210, isUninitialized: false, isLoading: true, isSuccess: false, isError: false, data: undefined, currentData: undefined, … }​
+currentData: undefined​
+data: undefined​
+endpointName: "getPosts"​
+isError: false​
+isFetching: true​
+isLoading: true​
+isSuccess: false​
+isUninitialized: false​
+refetch: function refetch()​
+requestId: "q62Jtu77cpI_T612SLuhD"​
+startedTimeStamp: 1696941177210​
+status: "pending"​
+<prototype>: Object { … }
+PostsList.js:13
+
+Object { status: "fulfilled", endpointName: "getPosts", requestId: "q62Jtu77cpI_T612SLuhD", startedTimeStamp: 1696941177210, data: (9) […], fulfilledTimeStamp: 1696941179299, isUninitialized: false, isLoading: false, isSuccess: true, isError: false, … }
+​currentData: Array(9) [ {…}, {…}, {…}, … ]​
+data: Array(9) [ {…}, {…}, {…}, … ]​
+endpointName: "getPosts"​
+fulfilledTimeStamp: 1696941179299​
+isError: false​
+isFetching: false​
+isLoading: false​
+isSuccess: true​
+isUninitialized: false​
+refetch: function refetch()​
+requestId: "q62Jtu77cpI_T612SLuhD"​
+startedTimeStamp: 1696941177210​
+status: "fulfilled"
+*/
