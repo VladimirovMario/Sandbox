ff66cfe1bc27e647fd618e2d3d8503ed6a10b4c2
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-11 15:43:35 +0300

    Transforming Responses
    Endpoints can define a transformResponse handler that can extract or modify the data received from the server before it's cached.

--- 02. Redux/02. Redux Documentation/02. Redux Essentials/redux-essentials-example-app-master/src/features/users/usersSlice.js
+++ 02. Redux/02. Redux Documentation/02. Redux Essentials/redux-essentials-example-app-master/src/features/users/usersSlice.js
@@ -1,47 +1,33 @@
-import { createSelector } from '@reduxjs/toolkit';
+import { createEntityAdapter, createSelector } from '@reduxjs/toolkit';
 import { apiSlice } from '../api/apiSlice';
 
-/* Temporarily ignore adapter - we'll use this again shortly
 const usersAdapter = createEntityAdapter();
 
 const initialState = usersAdapter.getInitialState();
-*/
+
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getUsers: builder.query({
+      query: () => '/users',
+      transformResponse: (responseData) => {
+        return usersAdapter.setAll(initialState, responseData);
+      },
+    }),
+  }),
+});
+
+export const { useGetUsersQuery } = extendedApiSlice;
 
 // Calling `someEndpoint.select(someArg)` generates a new selector that will return
 // the query result object for a query with those parameters.
 // To generate a selector for a specific query argument, call `select(theQueryArg)`.
 // In this case, the users query has no params, so we don't pass anything to select()
+export const selectUsersResult = apiSlice.endpoints.getUsers.select();
 
-const extendedApiSlice = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    getUsers: builder.query({
-      query: () => '/users',
-    }),
-  }),
-});
-
-const selectUsersResult = apiSlice.endpoints.getUsers.select();
-
-const emptyUsers = [];
-
-const selectAllUsers = createSelector(
+const selectUsersData = createSelector(
   selectUsersResult,
-  (usersResult) => usersResult?.data ?? emptyUsers
+  (usersResult) => usersResult.data
 );
 
-const selectUserById = createSelector(
-  selectAllUsers,
-  (state, userId) => userId,
-  (users, userId) => users.find((user) => user.id === userId)
-);
-
-export const { useGetUsersQuery } = extendedApiSlice;
-
-export { extendedApiSlice, selectUsersResult, selectAllUsers, selectUserById };
-
-/* Temporarily ignore selectors - we'll come back to this later
-export const {
-  selectAll: selectAllUsers,
-  selectById: selectUserById,
-} = usersAdapter.getSelectors((state) => state.users)
-*/
+export const { selectAll: selectAllUsers, selectById: selectUserById } =
+  usersAdapter.getSelectors((state) => selectUsersData(state) ?? initialState);
