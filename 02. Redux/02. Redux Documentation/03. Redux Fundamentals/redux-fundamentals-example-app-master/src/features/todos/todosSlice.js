c5689fe87ecac8a13d14a3fdd212e3795948c2f6
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-15 16:50:37 +0300

    Dispatching Actions

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/features/todos/todosSlice.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/features/todos/todosSlice.js
@@ -1,10 +1,8 @@
-const initialState = {
-  todos: [
-    { id: 0, text: 'Learn React', completed: true },
-    { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
-    { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
-  ],
-};
+const initialState = [
+  // { id: 0, text: 'Learn React', completed: true },
+  // { id: 1, text: 'Learn Redux', completed: false, color: 'purple' },
+  // { id: 2, text: 'Build something fun!', completed: false, color: 'blue' },
+];
 
 function nextTodoId(todos) {
   const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
