9a9910b4548c096b9290d9a43ff80fbbf00817e4
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-16 10:49:25 +0300

    Inside a Redux Store

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/store.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import rootReducer from './reducer';
 
 let preloadedState;
@@ -12,3 +12,35 @@ if (persistedTodosString) {
 
 const store = createStore(rootReducer, preloadedState);
 export default store;
+
+// It might be helpful to take a peek inside a Redux store to see how it works.
+// Here's a miniature example of a working Redux store, in about 25 lines of code:
+// This small version of a Redux store works well enough that you could use it to
+// replace the actual Redux createStore function you've been using in your app so far.
+// (Try it and see for yourself!) The actual Redux store implementation is longer and
+// a bit more complicated, but most of that is comments, warning messages, and handling some edge cases.
+function createStore(reducer, preloadedState) {
+  let state = preloadedState;
+  const listeners = [];
+
+  function getState() {
+    return state;
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+    return function unsubscribe() {
+      const index = listeners.indexOf(listener);
+      listeners.splice(index, 1);
+    };
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  }
+
+  dispatch({ type: '@@redux/INIT' });
+
+  return { dispatch, subscribe, getState };
+}
