9e8753bfedea1a43253be2075e5e725587e4c157
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-16 17:29:08 +0300

    React-Redux shallowEqual - React-Redux has a shallowEqual comparison function we can use to check if the items inside the array are still the same. - Now, if we toggle a todo item, the list of IDs will be considered the same, and <TodoList> won't have to re-render. - The one <TodoListItem> will get an updated todo object and re-render, but all the rest of them will still have the existing todo object and not have to re-render at all.

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/features/todos/TodoList.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/features/todos/TodoList.js
@@ -1,10 +1,10 @@
-import { useSelector } from 'react-redux';
+import { useSelector, shallowEqual } from 'react-redux';
 import TodoListItem from './TodoListItem';
 
 const selectTodoIds = (state) => state.todos.map((todo) => todo.id);
 
 export default function TodoList() {
-  const todoIds = useSelector(selectTodoIds);
+  const todoIds = useSelector(selectTodoIds, shallowEqual);
 
   const renderedListItems = todoIds.map((todoId) => {
     return <TodoListItem key={todoId} id={todoId} />;
