b2d696a50432445db221fadb317f8af3e85368ed
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-16 14:00:32 +0300

    Removed comments

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/store.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/store.js
@@ -2,53 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducer';
 import { loggerMiddleware } from './exampleAddons/loggerMiddleware';
-import { delayedMessageMiddleware } from './exampleAddons/delayedMessageMiddleware';
-
-const middlewareEnhancer = applyMiddleware(
-  loggerMiddleware,
-  delayedMessageMiddleware
-);
 
 const composedEnhancer = composeWithDevTools(
   // EXAMPLE: Add whatever middleware you actually want to use here
-  middlewareEnhancer
+  applyMiddleware(loggerMiddleware)
   // other store enhancers if any
 );
 
 const store = createStore(rootReducer, composedEnhancer);
 
 export default store;
-
-/*
-// It might be helpful to take a peek inside a Redux store to see how it works.
-// Here's a miniature example of a working Redux store, in about 25 lines of code:
-// This small version of a Redux store works well enough that you could use it to
-// replace the actual Redux createStore function you've been using in your app so far.
-// (Try it and see for yourself!) The actual Redux store implementation is longer and
-// a bit more complicated, but most of that is comments, warning messages, and handling some edge cases.
-function createStore(reducer, preloadedState) {
-  let state = preloadedState;
-  const listeners = [];
-
-  function getState() {
-    return state;
-  }
-
-  function subscribe(listener) {
-    listeners.push(listener);
-    return function unsubscribe() {
-      const index = listeners.indexOf(listener);
-      listeners.splice(index, 1);
-    };
-  }
-
-  function dispatch(action) {
-    state = reducer(state, action);
-    listeners.forEach((listener) => listener());
-  }
-
-  dispatch({ type: '@@redux/INIT' });
-
-  return { dispatch, subscribe, getState };
-}
-*/
