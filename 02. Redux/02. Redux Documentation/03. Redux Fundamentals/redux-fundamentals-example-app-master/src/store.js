03c7bb86a5df7f26a316f9385a68d1042beaa510
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-17 11:30:31 +0300

    Configuring the Store with the Redux thunk middleware - The Redux thunk middleware is available on NPM as a package called redux-thunk. We need to install that package to use it in our app

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/store.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-example-app-master/src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducer';
 import { loggerMiddleware } from './exampleAddons/loggerMiddleware';
 
 const composedEnhancer = composeWithDevTools(
   // EXAMPLE: Add whatever middleware you actually want to use here
-  applyMiddleware(loggerMiddleware)
+  applyMiddleware(loggerMiddleware, thunkMiddleware)
   // other store enhancers if any
 );
 
