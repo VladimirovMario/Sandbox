5fc3712b8b2a52dc57b48ea889e3c23343caf7bf
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-18 16:14:20 +0300

    Converting the Filters Reducer
    - Converting our filters slice file to use createSlice instead

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-modern-redux/src/features/filters/filtersSlice.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-modern-redux/src/features/filters/filtersSlice.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 export const StatusFilters = {
   All: 'all',
   Active: 'active',
@@ -9,54 +11,34 @@ const initialState = {
   colors: [],
 };
 
-export default function filtersReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'filters/statusFilterChanged': {
-      return {
-        // Again, one less level of nesting to copy
-        ...state,
-        status: action.payload,
-      };
-    }
-    case 'filters/colorFilterChanged': {
-      let { color, changeType } = action.payload;
-      const { colors } = state;
+const filtersSlice = createSlice({
+  name: 'filters',
+  initialState,
+  reducers: {
+    statusFilterChanged(state, action) {
+      state.status = action.payload;
+    },
+    colorFilterChanged: {
+      reducer(state, action) {
+        const { color, changeType } = action.payload;
+        const { colors } = state;
 
-      switch (changeType) {
-        case 'added': {
-          if (colors.includes(color)) {
-            // This color already is set as a filter. Don't change the state.
-            return state;
-          }
-
-          return {
-            ...state,
-            colors: state.colors.concat(color),
-          };
+        if (changeType === 'added' && colors.includes(color) === false) {
+          colors.push(color);
         }
-        case 'removed': {
-          return {
-            ...state,
-            colors: state.colors.filter(
-              (existingColor) => existingColor !== color
-            ),
-          };
+        if (changeType === 'removed') {
+          state.colors = colors.filter(
+            (existingColor) => existingColor !== color
+          );
         }
-        default:
-          return state;
-      }
-    }
-    default:
-      return state;
-  }
-}
-
-export const colorFilterChanged = (color, changeType) => ({
-  type: 'filters/colorFilterChanged',
-  payload: { color, changeType },
+      },
+      prepare(color, changeType) {
+        return { payload: { color, changeType } };
+      },
+    },
+  },
 });
 
-export const statusFilterChanged = (status) => ({
-  type: 'filters/statusFilterChanged',
-  payload: status,
-});
+export const { statusFilterChanged, colorFilterChanged } = filtersSlice.actions;
+
+export default filtersSlice.reducer;
