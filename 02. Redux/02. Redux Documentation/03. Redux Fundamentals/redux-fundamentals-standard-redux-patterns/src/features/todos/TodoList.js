f20493bfb2c3e2ffadfedb3ebe2fa0153737f906
VladimirovMario <mariovladimirovfs@gmail.com>
2023-10-18 10:12:04 +0300

    Showing a loading spinner

--- 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-standard-redux-patterns/src/features/todos/TodoList.js
+++ 02. Redux/02. Redux Documentation/03. Redux Fundamentals/redux-fundamentals-standard-redux-patterns/src/features/todos/TodoList.js
@@ -4,6 +4,15 @@ import TodoListItem from './TodoListItem';
 
 export default function TodoList() {
   const todoIds = useSelector(selectFilteredTodoIds);
+  const loadingStatus = useSelector((state) => state.todos.status);
+
+  if (loadingStatus === 'loading') {
+    return (
+      <div className="todo-list">
+        <div className="loader"></div>
+      </div>
+    );
+  }
 
   const renderedListItems = todoIds.map((todoId) => {
     return <TodoListItem key={todoId} id={todoId} />;
