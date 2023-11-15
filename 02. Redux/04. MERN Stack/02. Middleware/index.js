612a2ee15579da4f4c2c1ffa7282d621f52871c6
VladimirovMario <mariovladimirovfs@gmail.com>
2023-11-15 19:33:57 +0200

    Added Middleware

---
+++ 02. Redux/04. MERN Stack/02. Middleware/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+
+const port = process.env.PORT || 3300;
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+
+app.all('*', (req, res) => {
+  res.status(404);
+  // console.log(req.get('Accept'));
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Fond!' });
+  } else if (req.accepts('text/plain')) {
+    req.type('txt').send('404 Not Fond');
+  } else {
+    res.json({ message: '406 Not Acceptable' });
+  }
+});
+
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`You can make requests to: \nhttp://localhost:${port}`)
+);
