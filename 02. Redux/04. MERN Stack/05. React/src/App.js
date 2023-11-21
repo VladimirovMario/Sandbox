5499163ad49c5e0de713a279c40a6a0724289bf3
VladimirovMario <mariovladimirovfs@gmail.com>
2023-11-21 10:32:48 +0200

    Added React Router Dom - added base components

--- 02. Redux/04. MERN Stack/05. React/src/App.js
+++ 02. Redux/04. MERN Stack/05. React/src/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Public from './components/Public';
+import Login from './components/Login';
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
