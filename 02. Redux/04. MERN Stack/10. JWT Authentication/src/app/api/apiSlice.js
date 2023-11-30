296a5cd7fa8d7ef18edb5d2968f46a2b393c50f6
VladimirovMario <mariovladimirovfs@gmail.com>
2023-11-30 19:40:59 +0200

    🔒 Enhance API calls with token refresh
    - Updated the API configuration to include token handling with automatic refresh on receiving a 403 status code.
    - The base query now includes a mechanism to request a new token and retry the initial query, ensuring a seamless authentication experience.
    - Also, improved error handling for expired logins, logging users out when needed.

--- 02. Redux/04. MERN Stack/10. JWT Authentication/src/app/api/apiSlice.js
+++ 02. Redux/04. MERN Stack/10. JWT Authentication/src/app/api/apiSlice.js
@@ -1,9 +1,52 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { logOut, setCredentials } from '../../features/auth/authSlice';
 
 const baseUrl = 'http://localhost:3300';
 
+const baseQuery = fetchBaseQuery({
+  baseUrl,
+  credentials: 'include',
+  prepareHeaders: (headers, { getState, extra, endpoint, type, forced }) => {
+    const token = getState().auth.token;
+
+    // If we have a token set in state, let's assume that we should be passing it.
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`);
+    }
+
+    return headers;
+  },
+});
+
+const baseQueryWithReAuth = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+
+  // If you want, handle other status codes, too
+  if (result.error && result.error.status === 403) {
+    // try to get a new token
+    const refreshResult = await baseQuery('/auth/refresh', api, extraOptions);
+
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+
+      // retry the initial query
+      // console.log('sending refresh token');
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      if (refreshResult?.error?.status === 403) {
+        refreshResult.error.data.message = 'Your login has expired. ';
+        api.dispatch(logOut());
+      }
+      return refreshResult;
+    }
+  }
+
+  return result;
+};
+
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: baseQueryWithReAuth,
   tagTypes: ['Note', 'User'],
   endpoints: (builder) => ({}),
 });
