955bd52e4e4e8901ae619745b1e78a51fbc6490e
VladimirovMario <mariovladimirovfs@gmail.com>
2023-12-05 15:18:14 +0200

    Added staggeredBaseQuery

--- 02. Redux/04. MERN Stack/10. JWT Authentication/src/app/api/apiSlice.js
+++ 02. Redux/04. MERN Stack/10. JWT Authentication/src/app/api/apiSlice.js
@@ -1,30 +1,38 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { /* logOut, */ setCredentials } from '../../features/auth/authSlice';
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
+import { /*logOut, */ setCredentials } from '../../features/auth/authSlice';
 
 const baseUrl = 'http://localhost:3300';
 
-const baseQuery = fetchBaseQuery({
-  baseUrl,
-  credentials: 'include',
-  prepareHeaders: (headers, { getState, extra, endpoint, type, forced }) => {
-    const token = getState().auth.token;
+// https://redux-toolkit.js.org/rtk-query/usage/customizing-queries#automatic-retries
+const staggeredBaseQuery = retry(
+  fetchBaseQuery({
+    baseUrl,
+    credentials: 'include',
+    prepareHeaders: (headers, { getState, extra, endpoint, type, forced }) => {
+      const token = getState().auth.token;
 
-    // If we have a token set in state, let's assume that we should be passing it.
-    if (token) {
-      headers.set('authorization', `Bearer ${token}`);
-    }
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
 
-    return headers;
-  },
-});
+      return headers;
+    },
+  }),
+  { maxRetries: 0 }
+);
 
 const baseQueryWithReAuth = async (args, api, extraOptions) => {
-  let result = await baseQuery(args, api, extraOptions);
+  let result = await staggeredBaseQuery(args, api, extraOptions);
 
   // If you want, handle other status codes, too
   if (result.error && result.error.status === 403) {
     // try to get a new token
-    const refreshResult = await baseQuery('/auth/refresh', api, extraOptions);
+    const refreshResult = await staggeredBaseQuery(
+      '/auth/refresh',
+      api,
+      extraOptions
+    );
 
     if (refreshResult?.data) {
       // store the new token
@@ -32,7 +40,7 @@ const baseQueryWithReAuth = async (args, api, extraOptions) => {
 
       // retry the initial query
       // console.log('sending refresh token');
-      result = await baseQuery(args, api, extraOptions);
+      result = await staggeredBaseQuery(args, api, extraOptions);
     } else {
       if (refreshResult?.error?.status === 403) {
         refreshResult.error.data.message = 'Your login has expired. ';
