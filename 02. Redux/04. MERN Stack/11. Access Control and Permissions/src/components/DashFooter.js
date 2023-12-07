4cad50f54a3de9424cbb4ca9eda03c9f0f65e1ed
VladimirovMario <mariovladimirovfs@gmail.com>
2023-12-07 17:07:13 +0200

    feat: Integrate useAuth hook in Welcome and DashFooter components
    - Utilize useAuth hook to retrieve and display user information in Welcome.
    - Display current user and status in DashFooter using useAuth.
    - Improve navigation with a "Go Home" button in DashFooter.

--- 02. Redux/04. MERN Stack/11. Access Control and Permissions/src/components/DashFooter.js
+++ 02. Redux/04. MERN Stack/11. Access Control and Permissions/src/components/DashFooter.js
@@ -1,13 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate, useLocation } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const DashFooter = () => {
+  // Use the useAuth hook to get current user information and status
+  const { username, status } = useAuth();
+
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
+  // Handler for navigating to the home page
   const onGoHomeClicked = () => navigate('/dash');
 
+  // Conditional rendering of the "Go Home" button based on the current path
   let goHomeButton = <></>;
   if (pathname !== '/dash') {
     goHomeButton = (
@@ -21,11 +27,12 @@ const DashFooter = () => {
     );
   }
 
+  // Content to be rendered in the DashFooter component
   const content = (
     <footer className="dash-footer">
       {goHomeButton}
-      <p>Current User:</p>
-      <p>Status:</p>
+      <p>Current User: {username}</p>
+      <p>Status: {status}</p>
     </footer>
   );
 
--- 02. Redux/04. MERN Stack/11. Access Control and Permissions/src/features/auth/Welcome.js
+++ 02. Redux/04. MERN Stack/11. Access Control and Permissions/src/features/auth/Welcome.js
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
 
 const Welcome = () => {
+  // Retrieve user information and roles using the useAuth hook
+  const { username, isManager, isAdmin } = useAuth();
+
   const date = new Date();
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
-  // British English uses day-month-year order
+  // Get the current date and format it in British English style
   const options = {
     dateStyle: 'full',
     timeStyle: 'long',
@@ -12,22 +16,27 @@ const Welcome = () => {
   const today = new Intl.DateTimeFormat('en-GB', options).format(date);
   // Tuesday, 21 November 2023 at 11:32:23 GMT+2
 
+  // Content to be rendered in the Welcome component
   const content = (
     <section className="welcome">
       <p>{today}</p>
-      <h1>Welcome</h1>
+      <h1>Welcome {username}</h1>
       <p>
         <Link to="/dash/notes">View Tech Notes</Link>
       </p>
       <p>
         <Link to="/dash/notes/new">Add New Tech Notes</Link>
       </p>
-      <p>
-        <Link to="/dash/users">View User Settings</Link>
-      </p>
-      <p>
-        <Link to="/dash/users/new">Add New User</Link>
-      </p>
+      {(isAdmin || isManager) && (
+        <p>
+          <Link to="/dash/users">View User Settings</Link>
+        </p>
+      )}
+      {(isAdmin || isManager) && (
+        <p>
+          <Link to="/dash/users/new">Add New User</Link>
+        </p>
+      )}
     </section>
   );
   return content;
