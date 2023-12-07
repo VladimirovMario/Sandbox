095d63fc7c607c3cfbdc00b0b3cf1445fd0ca777
VladimirovMario <mariovladimirovfs@gmail.com>
2023-12-07 18:39:02 +0200

    feat: Enhance DashHeader with dynamic buttons and improved navigation
    - Added dynamic navigation buttons based on the user's role and current route.
    - Improved the navigation logic to include buttons for creating new notes and users.
    - Enhanced error handling and display during logout.

--- 02. Redux/04. MERN Stack/11. Access Control and Permissions/src/components/DashHeader.js
+++ 02. Redux/04. MERN Stack/11. Access Control and Permissions/src/components/DashHeader.js
@@ -1,15 +1,25 @@
 import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFileCirclePlus,
+  faFilePen,
+  faUserGear,
+  faUserPlus,
+  faRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 
 import { useSendLogoutMutation } from '../features/auth/authApiSlice';
+
+import useAuth from '../hooks/useAuth';
 
 const DASH_REGEX = /^\/dash(\/)?$/;
 const NOTES_REGEX = /^\/dash\/notes(\/)?$/;
 const USERS_REGEX = /^\/dash\/users(\/)?$/;
 
 const DashHeader = () => {
+  // Retrieve user information and roles using the useAuth hook
+  const { isAdmin, isManager } = useAuth();
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -22,18 +32,17 @@ const DashHeader = () => {
     }
   }, [isSuccess, navigate]);
 
-  const onLogOutClicked = () => {
-    sendLogout();
-    navigate('/');
-  };
+  // Function to handle the click event for the "New Note" button
+  const onNewNoteClicked = () => navigate('/dash/notes/new');
+  // Function to handle the click event for the "New User" button
+  const onNewUserClicked = () => navigate('/dash/users/new');
+  // Function to handle the click event for the "Users" button
+  const onUsersClicked = () => navigate('/dash/users');
+  // Function to handle the click event for the "Notes" button
+  const onNotesClicked = () => navigate('/dash/notes');
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (isError) {
-    return <p>Error: {error.data?.message}</p>;
-  }
+  // Function to handle the click event for the "Logout" button
+  const onLogOutClicked = () => sendLogout();
 
   let dashClass = null;
   if (
@@ -44,24 +53,93 @@ const DashHeader = () => {
     dashClass = 'dash-header__container--small';
   }
 
+  // Conditional rendering for the "New Note" button based on the current route
+  let newNoteButton = null;
+  if (NOTES_REGEX.test(pathname)) {
+    newNoteButton = (
+      <button
+        className="icon-button"
+        title="New Note"
+        onClick={onNewNoteClicked}
+      >
+        <FontAwesomeIcon icon={faFileCirclePlus} />
+      </button>
+    );
+  }
+
+  // Conditional rendering for the "New User" button based on the current route
+  let newUserButton = null;
+  if (USERS_REGEX.test(pathname)) {
+    newUserButton = (
+      <button
+        className="icon-button"
+        title="New User"
+        onClick={onNewUserClicked}
+      >
+        <FontAwesomeIcon icon={faUserPlus} />
+      </button>
+    );
+  }
+
+  // Conditional rendering for the "Users" button based on user roles and the current route
+  let userButton = null;
+  if (isManager || isAdmin) {
+    if (USERS_REGEX.test(pathname) === false && pathname.includes('/dash')) {
+      userButton = (
+        <button className="icon-button" title="Users" onClick={onUsersClicked}>
+          <FontAwesomeIcon icon={faUserGear} />
+        </button>
+      );
+    }
+  }
+
+  // Conditional rendering for the "Notes" button based on the current route
+  let notesButton = null;
+  if (NOTES_REGEX.test(pathname) === false && pathname.includes('/dash')) {
+    notesButton = (
+      <button className="icon-button" title="Notes" onClick={onNotesClicked}>
+        <FontAwesomeIcon icon={faFilePen} />
+      </button>
+    );
+  }
+
   const logoutButton = (
     <button className="icon-button" title="Logout" onClick={onLogOutClicked}>
       <FontAwesomeIcon icon={faRightFromBracket} />
     </button>
   );
 
+  // Conditional rendering for the content based on loading state
+  let buttonContent = <></>;
+  if (isLoading) {
+    buttonContent = <p>Logging Out...</p>;
+  } else {
+    buttonContent = (
+      <>
+        {newNoteButton}
+        {newUserButton}
+        {notesButton}
+        {userButton}
+        {logoutButton}
+      </>
+    );
+  }
+
+  const errClass = isError ? 'errmsg' : 'offscreen';
+
   const content = (
-    <header className="dash-header">
-      <div className={`dash-header__container ${dashClass}`}>
-        <Link to="/dash">
-          <h1 className="dashe-header__title">Tech notes</h1>
-        </Link>
-        <nav className="dash-header__nav">
-          {/* add more buttons later*/}
-          {logoutButton}
-        </nav>
-      </div>
-    </header>
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <header className="dash-header">
+        <div className={`dash-header__container ${dashClass}`}>
+          <Link to="/dash">
+            <h1 className="dashe-header__title">Tech notes</h1>
+          </Link>
+          <nav className="dash-header__nav">{buttonContent}</nav>
+        </div>
+      </header>
+    </>
   );
 
   return content;
