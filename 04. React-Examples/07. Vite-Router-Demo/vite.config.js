3ea6ba4a0e88eb3f9276274bc2ec0859b0759058
Mario <mariovladimirovfs@gmail.com>
2025-06-03 09:30:18 +0300

    feat: add advanced Vite config with custom logger and async setup
    - Suppress CSS warnings by overriding logger.warn
    - Log Vite config context (command, mode, SSR, preview)
    - Simulate async setup with delayForDemo() during config
    - Dynamically change base path in production mode

--- 04. React-Examples/07. Vite-Router-Demo/vite.config.js
+++ 04. React-Examples/07. Vite-Router-Demo/vite.config.js
@@ -1,7 +1,45 @@
-import { defineConfig } from 'vite';
+import { createLogger, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(async ({ command, mode, isSsrBuild, isPreview }) => {
+    const logger = createLogger();
+    const loggerWarn = logger.warn;
+
+    logger.warn = (msg, options) => {
+        // Ignore empty CSS files warning
+        if (msg.includes('vite:css') && msg.includes(' is empty')) return;
+        loggerWarn(msg, options);
+    };
+
+    const config = {
+        base: '/',
+        plugins: [react()],
+        customLogger: logger,
+    };
+
+    if (mode === 'production') {
+        config.base = '/vite/';
+    }
+
+    console.group('Configuring Vite');
+    console.info('command', command);
+    console.info('mode', mode);
+    console.info('isSsrBuild', isSsrBuild);
+    console.info('isPreview', isPreview);
+    console.info('async data', await delayForDemo());
+    console.info('createLogger', logger);
+    console.groupEnd();
+
+    return config;
 });
+
+function delayForDemo() {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+    }).then(() => {
+        return {
+            apiCall: 'Important information',
+        };
+    });
+}
