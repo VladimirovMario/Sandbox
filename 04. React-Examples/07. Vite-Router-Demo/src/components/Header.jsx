3dd25501d7003d5daa517ee3ed99fd80536584e5
Mario <mariovladimirovfs@gmail.com>
2025-06-05 18:04:08 +0300

    style(header): apply scoped styles using CSS Modules to demonstrate integration with Vite
    - Created Header.module.css with basic layout and visual styles
    - Imported styles using Vite's native support for CSS Modules
    - Applied styles locally via `className={styles['class-name']}`
    
    This serves as an example of how simple CSS Modules integration is with Vite.

--- 04. React-Examples/07. Vite-Router-Demo/src/components/Header.jsx
+++ 04. React-Examples/07. Vite-Router-Demo/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import Navigation from './Navigation';
+import { Link } from '../router/reactRouter';
+import reactLogo from '../assets/react.svg';
+import styles from './Header.module.css';
 
 export default function Header() {
     return (
         <header>
-            <nav className='header-nav'>
+            <nav className={styles['header-nav']}>
+                <Link to="/">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </Link>
                 <Navigation />
             </nav>
         </header>
