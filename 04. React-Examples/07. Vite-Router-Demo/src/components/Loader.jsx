dfab830a23ac59fba179c8cd91a9bf87ae1c9a13
Mario <mariovladimirovfs@gmail.com>
2025-06-13 09:22:08 +0300

    chore: fix warning for not using className

--- 04. React-Examples/07. Vite-Router-Demo/src/components/Loader.jsx
+++ 04. React-Examples/07. Vite-Router-Demo/src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import styles from './Loader.module.css';
 
 export default function Loader() {
-    return <div class={styles['loading-text']}>Loading...</div>;
+    return <div className={styles['loading-text']}>Loading...</div>;
 }
